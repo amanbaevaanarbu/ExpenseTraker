b492a4bc51aab702a8d8900d799746dcd9a0a7fb
amanbaevaanarbu <amaanarbu633@gmail.com>
2023-04-07 14:38:04 +0300

    expense

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+import Expense from './components/expense/Expense'
+import NewExpense from './components/new-expense/NewExpense'
+
+function App() {
+	const [expenses, setExpenses] = useState([
+		{
+			title: 'Новый телевизор',
+			price: 400,
+			date: new Date(),
+		},
+		{
+			title: 'Новый iphone',
+			price: 600,
+			date: new Date(),
+		},
+	])
+	const addExpense = (item) => {
+		setExpenses([...expenses, item])
+	}
+	return (
+		<div className='App'>
+			<NewExpense expenseFunc={addExpense} />
+			<Expense expenses={expenses} />
+		</div>
+	)
+}
+
+
+// React
+// component
+//1.function
+//2. return jsx
+//3. componenttin aty chon tamga menen bashtalysh kerek
+// reactive(state, Vdom)
+
+//////////////////////////////////////////////////////////////
+/// state
+//render
+// each component has own state
+// chldren
+// keys
+export default App
